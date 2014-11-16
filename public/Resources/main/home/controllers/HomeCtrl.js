4fcfcd21042c5e5d924b01507bd67f6f402b8f62
srbhklntr <saurabh.kalantri@yahoo.com>
2014-11-16 00:02:58 -0800

    Added data into home.json and made the changes in quotes.html

---
+++ public/Resources/main/home/controllers/HomeCtrl.js
@@ -0,0 +1,19 @@
+var junoonFoundation = angular.module('junoonFoundation');
+junoonFoundation.controller('HomeCtrl', function($http, $scope, $rootScope, $route){
+	$rootScope.init = function(){
+		console.log("Control comes to HomeCtrl");
+		var url = "Resources/JSON/home.json";
+		$http({
+			url: url,
+			method: 'GET',
+			headers: {
+				'content-type':'application/json'
+			}
+		}).success(function(data,status,headers,config){
+			$rootScope.home=data;
+			console.log($rootScope.home);
+		}).error(function(data, status, headers, config){
+			console.log("Error comes");
+		});
+	} 
+});
