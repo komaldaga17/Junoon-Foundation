894d7181434b422408a2ee0c74bb37e6015ef1c6
srbhklntr <saurabh.kalantri@yahoo.com>
2014-11-15 22:20:56 -0800

    Added angular module to the controller

--- public/Resources/main/application/Mainctrl.js
+++ public/Resources/main/application/Mainctrl.js
@@ -1,3 +1,8 @@
-(function() {
 
-});
+	var junoonFoundation = angular.module('junoonFoundation');
+	junoonFoundation.controller('Mainctrl', function($http, $scope, $rootScope, $route){
+		$rootScope.initMain = function(){
+			console.log("Saurabh Runs his first Angular App");
+		}
+	});
+
