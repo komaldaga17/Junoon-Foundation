0a18a740ebdf3ffd81e10d7f671c264d998e946f
srbhklntr <saurabh.kalantri@yahoo.com>
2015-03-18 16:25:11 -0700

    Added some server side code to edit the quotes

--- app.js
+++ app.js
@@ -1,9 +1,47 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var request = require('request');
+
+// request('/Resources/JSON/home.json', function(err, resp){
+// 	if(resp.statusCode === 200){
+// 		console.log("I am there at the location");
+// 	}
+// })
 
 var app = express();
-
+app.use(bodyParser.json());
+console.log(__dirname);
 app.use(express.static(__dirname + '/public'));
 
-var port = process.env.PORT || 8000;
 
+var port = process.env.PORT || 8000;
+console.log(app.post);3
+var payload = [];
+app.post('/getUrl', function(req,res){
+	console.log("I am in server");
+	// console.log(JSON.parse(req));
+	// console.log(JSON.parse(req).name);
+	var data = req.body;
+	console.log(data);
+	console.log(data[0].name);
+	editJSON(data);	
+});
+
+
+function editJSON(data){
+	var obj;
+	fs.readFile('public/Resources/JSON/home.json', 'utf8', function(err,data){
+		console.log("I am reading file");
+		// console.log(data);
+		// console.log(obj);
+		// console.log(home.quotes);
+		console.log("Content : "  + data);
+		console.log(typeof(data));
+		console.log((JSON.parse(data)).quotes);
+
+	});
+	fs.lstatSync('public/Resources/JSON/home.json').isFile();
+	
+}
 app.listen(port);
--- public/Resources/edit/home/controllers/EditHomeCtrl.js
+++ public/Resources/edit/home/controllers/EditHomeCtrl.js
@@ -16,7 +16,6 @@ junoonFoundation.controller('EditHomeCtrl', ['$scope', '$http', '$rootScope', '$
 		}
 		console.log($scope.quotesList);
 		console.log(typeof($scope.quotesList));
-
 	}
 	$scope.open = function(index, element){
 		console.log(index);
@@ -29,17 +28,39 @@ junoonFoundation.controller('EditHomeCtrl', ['$scope', '$http', '$rootScope', '$
 	$scope.submit = function(){
 		alert("I got clicked ahhhh");
 		var requestObject = [];
+		var url = "Resources/JSON/home.json";
 		for(var key=0 in $scope.quotesList){
 			if($scope.quotesList[key].name !== $rootScope.home.quotes[key].name){
 				console.log("They are different");
 				requestObject.push($scope.quotesList[key]);
-				console.log(requestObject);
-				console.log(JSON.stringify(requestObject));
+				console.log(requestObject);		
 			}else{
 				console.log("They are Different");
 			}
 		}
+		$http({
+			method : 'POST',
+			url	: 'getUrl',
+			data : JSON.stringify(requestObject),
+			headers : {'Content-Type' : 'application/json'}			
+			}).success(function(data, status, headers, config){
+				console.log("This is will execute when there is some response");
+			}).error(function(data, status, headers, config){
+				console.log("Error Comes while editing the stuff");
+			});
 
+		// }
+		/*$http({
+			url : url,
+			method : 'POST',
+			headers : {
+				'Content-Type' : 'application/json'
+			}
+		}).success(function(data,status,headers,config){
+			$rootScope.home=data;
+			console.log($rootScope.home.quotes);
+		}).error(function(data, status, headers, config){
+			console.log("Error comes");
+		})*/
 	}
-	
 }]);
