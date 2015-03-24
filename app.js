31bacbdf596c55e344d9ab0377e9f27368e57434
srbhklntr <saurabh.kalantri@yahoo.com>
2015-03-23 17:31:48 -0700

    Removed the public stuff to main server and added routes directory to separate the work for home for now

--- app.js
+++ app.js
@@ -1,46 +1,51 @@
 'use strict';
 
 var express = require('express');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 var fs = require('fs');
-var request = require('request');
-
+// var request = require('request');
 var app = express();
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 console.log(__dirname);
 app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 8000;
 
-app.post('/getUrl', function(req,res){
-	console.log("I am in server");
-	console.log(req.url);
-	console.log(res.statusCode);
-	var data = req.body;	
-	editJSON(data);
-	res.send("I got into the success of the api");
-	res.end();	
-});
-function editJSON(data){
-	var obj;
-	var newData = data;
-	fs.readFile('public/Resources/JSON/home.json', 'utf8', function(err,data){
-		var existingObject = (JSON.parse(data));
-		for(var key=0 in newData){
-			if(newData[key] == null){
-				existingObject.quotes[key].name = existingObject.quotes[key].name;
-				existingObject.quotes[key].content = existingObject.quotes[key].content;
+var home = require('./routes/home/home.js');
+app.use(home);
 
-			}else{
-				existingObject.quotes[key].name = newData[key].name;
-				existingObject.quotes[key].content = newData[key].content;
-			}
+// require('./routes/home/home.js')(app);
+
+
+// app.post('/getUrl', function(req,res){
+// 	console.log("I am in server");
+// 	console.log(req.url);
+// 	console.log(res.statusCode);
+// 	var data = req.body;	
+// 	editJSON(data);
+// 	res.send("I got into the success of the api");
+// 	res.end();	
+// });
+// function editJSON(data){
+// 	var obj;
+// 	var newData = data;
+// 	fs.readFile('public/Resources/JSON/home.json', 'utf8', function(err,data){
+// 		var existingObject = (JSON.parse(data));
+// 		for(var key=0 in newData){
+// 			if(newData[key] == null){
+// 				existingObject.quotes[key].name = existingObject.quotes[key].name;
+// 				existingObject.quotes[key].content = existingObject.quotes[key].content;
+
+// 			}else{
+// 				existingObject.quotes[key].name = newData[key].name;
+// 				existingObject.quotes[key].content = newData[key].content;
+// 			}
 						
-		}		
-		writeFile(existingObject);
-	});	
-}
-function writeFile(existingObject){
-		fs.writeFileSync('public/Resources/JSON/home.json', JSON.stringify(existingObject));
-}
+// 		}		
+// 		writeFile(existingObject);
+// 	});	
+// }
+// function writeFile(existingObject){
+// 		fs.writeFileSync('public/Resources/JSON/home.json', JSON.stringify(existingObject));
+// }
 app.listen(port);
---
+++ routes/home/home.js
@@ -0,0 +1,41 @@
+'use strict';
+
+
+var express = require('express');
+var app = module.exports = express();
+var fs = require('fs');
+
+console.log("I am into the route");
+
+app.post('/getUrl', function(req,res){
+	console.log("I am in server");
+	console.log(req.url);
+	console.log(res.statusCode);
+	var data = req.body;	
+	editJSON(data);
+	res.send("I got into the success of the api");
+	res.end();	
+});
+
+function editJSON(data){
+	var obj;
+	var newData = data;
+	fs.readFile('public/Resources/JSON/home.json', 'utf8', function(err,data){
+		var existingObject = (JSON.parse(data));
+		for(var key=0 in newData){
+			if(newData[key] == null){
+				existingObject.quotes[key].name = existingObject.quotes[key].name;
+				existingObject.quotes[key].content = existingObject.quotes[key].content;
+
+			}else{
+				existingObject.quotes[key].name = newData[key].name;
+				existingObject.quotes[key].content = newData[key].content;
+			}
+						
+		}		
+		writeFile(existingObject);
+	});	
+};
+function writeFile(existingObject){
+		fs.writeFileSync('public/Resources/JSON/home.json', JSON.stringify(existingObject));
+};
