62f669ffe92a9b6afe6f96bd6a2f8209a3446f70
srbhklntr <saurabh.kalantri@yahoo.com>
2015-03-23 17:36:37 -0700

    Removed unwanted comment form the main server app,js

--- app.js
+++ app.js
@@ -1,51 +1,17 @@
 'use strict';
 
 var express = require('express');
-// var bodyParser = require('body-parser');
-var fs = require('fs');
-// var request = require('request');
 var app = express();
-// app.use(bodyParser.json());
+var fs = require('fs');
+
 console.log(__dirname);
 app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 8000;
 
 var home = require('./routes/home/home.js');
+
 app.use(home);
 
-// require('./routes/home/home.js')(app);
 
-
-// app.post('/getUrl', function(req,res){
-// 	console.log("I am in server");
-// 	console.log(req.url);
-// 	console.log(res.statusCode);
-// 	var data = req.body;	
-// 	editJSON(data);
-// 	res.send("I got into the success of the api");
-// 	res.end();	
-// });
-// function editJSON(data){
-// 	var obj;
-// 	var newData = data;
-// 	fs.readFile('public/Resources/JSON/home.json', 'utf8', function(err,data){
-// 		var existingObject = (JSON.parse(data));
-// 		for(var key=0 in newData){
-// 			if(newData[key] == null){
-// 				existingObject.quotes[key].name = existingObject.quotes[key].name;
-// 				existingObject.quotes[key].content = existingObject.quotes[key].content;
-
-// 			}else{
-// 				existingObject.quotes[key].name = newData[key].name;
-// 				existingObject.quotes[key].content = newData[key].content;
-// 			}
-						
-// 		}		
-// 		writeFile(existingObject);
-// 	});	
-// }
-// function writeFile(existingObject){
-// 		fs.writeFileSync('public/Resources/JSON/home.json', JSON.stringify(existingObject));
-// }
 app.listen(port);
